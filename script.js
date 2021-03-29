4e1e1eca8d6b97e56ea4a51e6182c6b061f4e49c
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-03-29 21:47:21 +1100

    removed template html for time block. Added function to construct time block html dynamically

--- script.js
+++ script.js
@@ -11,9 +11,48 @@ var hourArray = [
 ]
 
 var now = moment().format("H");
-console.log(now);
-for (i in hourArray) {
-    var dt = moment(hourArray[i], ["hA"]).format("H");
 
-    console.log(dt)
+function constructTimeBlock(currentHour, thisHour24, thisHourName) {
+    
+
+    var timeBlockDiv = $("<div />");
+    timeBlockDiv.addClass("row time-block");
+
+    //Construct div to hold hour name
+    var hourDiv = $("<div />");
+    hourDiv.addClass("col-sm-1 hour");
+    hourDiv.append(thisHourName);
+    timeBlockDiv.append(hourDiv);
+
+    //Construct div to hold saved tasks
+    var descriptionDiv = $("<div />");
+    console.log(currentHour);
+    console.log(thisHour24);
+    if (thisHour24 < currentHour) {
+        descriptionDiv.addClass("col-sm-10 past");
+    } else if (thisHour24 == currentHour) {
+        descriptionDiv.addClass("col-sm-10 present");
+    } else {
+        descriptionDiv.addClass("col-sm-10 future");
+    }
+    timeBlockDiv.append(descriptionDiv);
+
+    //Construct Div to hold save button
+    var saveDiv = $("<div />");
+    saveDiv.addClass("saveBtn col-sm-1");
+    var saveIcon = $("<I />");
+    saveIcon.addClass("fas fa-save");
+    saveDiv.append(saveIcon);
+    //<i class="fas fa-save"></i>
+    timeBlockDiv.append(saveDiv);
+
+    var container = $(".container")[0];
+    container.append(timeBlockDiv[0]);
+
 }
+
+for (i in hourArray) {
+    var dt = moment(hourArray[i], ["hA"]).format("HH");
+    constructTimeBlock(now, dt, hourArray[i]);
+}
+
